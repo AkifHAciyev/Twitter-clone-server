ef5d3de951efbdd7967c34556cbd3e2774d1435e
AkifHaciyev <akif6563@mail.ru>
2023-03-02 23:54:27 +0400

    add userAuth, use express-validator,jsonwebtoken,bcrypt

--- index.js
+++ index.js
@@ -1,9 +1,62 @@
 import express from 'express';
+import { validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import { registerValidation } from './validations.js';
+import UserModel from './models/User.js';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('hello');
+mongoose
+	.connect('mongodb+srv://Akif:Akif1995@cluster0.g6v2zjb.mongodb.net/tweeter')
+	.then(() => console.log('db ok'))
+	.catch((err) => console.log(err));
+
+app.post('/auth/register', registerValidation, async (req, res) => {
+	try {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json(errors.array());
+		}
+
+		const password = req.body.password;
+		const salt = await bcrypt.genSalt(10);
+		const hash = await bcrypt.hash(password, salt);
+
+		const doc = new UserModel({
+			email: req.body.email,
+			fullName: req.body.fullName,
+			avatarUrl: req.body.avatarUrl,
+			passwordHash: hash,
+		});
+
+		const user = await doc.save();
+
+		const token = jwt.sign(
+			{
+				_id: user._id,
+			},
+			'secret123',
+			{
+				expiresIn: '30d',
+			}
+		);
+
+		const { passwordHash, ...userData } = user._doc;
+
+		res.json({
+			...userData,
+			token,
+		});
+	} catch (err) {
+		console.log(err);
+
+		res.status(500).json({
+			message: 'Failed to register',
+		});
+	}
 });
 
 app.listen(8080, (err) => {
---
+++ validations.js
@@ -0,0 +1,8 @@
+import { body } from 'express-validator';
+
+export const registerValidation = [
+	body('email', 'Invalid mail format').isEmail(),
+	body('password', 'Password must be at least 5 characters').isLength({ min: 5 }),
+	body('fullName', 'Enter a name').isLength({ min: 2 }),
+	body('avatarUrl', 'Invalid avatar link').optional().isURL(),
+];
