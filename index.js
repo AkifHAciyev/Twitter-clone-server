e46600f882652d49773921e7536ba41a6acc76c0
AkifHaciyev <akif6563@mail.ru>
2023-03-04 04:24:01 +0400

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import multer from 'multer';
+import cors from 'cors';
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 import { userController, postController } from './controllers/index.js';
 import { checkAuth, handleValidationErrors } from './utils/index.js';
@@ -22,8 +23,8 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
-
 app.use(express.json());
+app.use(cors());
 app.use('/upload', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, handleValidationErrors, userController.login);
