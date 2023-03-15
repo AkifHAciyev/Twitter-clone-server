78e7697c2378e572c67f00ec9835b84984e279b4
AkifHaciyev <akif6563@mail.ru>
2023-03-15 04:22:26 +0400

    deploy

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(__dirname +'/uploads/'));
 
 app.post('/auth/login', loginValidation, handleValidationErrors, userController.login);
 app.post('/auth/register', registerValidation, handleValidationErrors, userController.register);
